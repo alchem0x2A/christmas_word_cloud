b9fb3f347a0b315f4a1079cc2958911e313e2794
Tian Tian <lovaulonzezoeta@gmail.com>
2018-11-22 11:58:52 +0100

    Update the word list and script

--- print_text.py
+++ print_text.py
@@ -15,21 +15,22 @@ seed()
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111)
 with open("./freq_words.txt", "r") as f:
-    words = list(map(lambda s: s.strip(), f.readlines()))
+    words = list(map(lambda s: s.strip().upper(), f.readlines()))
 
 rows = math.ceil(math.sqrt(len(words)))
 for i in range(rows):
     for j in range(len(words) // rows):
         index = i * rows + j
-        w = words[index]
-        fontsize = randint(12, 20)
-        x = 1 / rows * i
-        y = 1 / rows * j
-        prop.set_size(fontsize)
-        ax.text(x=x, y=y, s=w,
-                size=fontsize,
-                transform=ax.transAxes,
-                fontproperties=prop)
+        if index < len(words):
+            w = words[index]
+            fontsize = randint(12, 20)
+            x = 1 / rows * i
+            y = 1 / (len(words) // rows) * j
+            prop.set_size(fontsize)
+            ax.text(x=x, y=y, s=w,
+                    size=fontsize,
+                    transform=ax.transAxes,
+                    fontproperties=prop)
 
 ax.set_axis_off()
 fig.savefig("test.svg")
